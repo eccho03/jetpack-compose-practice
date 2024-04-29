5035e4414ec57f240986da541043ccc4040e3b5d
choeunchae <choeunchae@ajou.ac.kr>
2024-04-29 23:06:47 +0900

    ui practice - button/toast

--- app/src/main/java/com/example/jetpack1/MainActivity.kt
+++ app/src/main/java/com/example/jetpack1/MainActivity.kt
@@ -1,16 +1,23 @@
 package com.example.jetpack1
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -24,13 +31,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Jetpack1Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    MyTextEx()
-                }
+                MyBtn()
             }
         }
     }
@@ -52,22 +53,34 @@ fun MyTextEx() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MyBtn() {
+    val context = LocalContext.current
+
+    Button(onClick = {
+        Log.d("Main", "onClick")
+        Toast.makeText(context, "클릭완료", Toast.LENGTH_LONG).show()
+    },
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color.Yellow,
+            contentColor = Color.Blue
+        ),
+        modifier = Modifier
+            .width(200.dp)
+            .height(300.dp)
+        ) {
+            Text(
+                text = "버튼버튼 버튼입니다 버튼버튼버튼버튼",
+                Modifier.padding(vertical = 50.dp),
+                fontSize = 30.sp,
+                color = Color.Red
+            )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Jetpack1Theme {
-        Surface(
-            modifier = Modifier.fillMaxSize(),
-            color = MaterialTheme.colorScheme.background
-        ) {
-            MyTextEx()
-        }
+        MyBtn()
     }
 }
