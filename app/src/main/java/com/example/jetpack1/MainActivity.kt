3e6925b426a175834b75eb49091862ab5bc33c75
choeunchae <choeunchae@ajou.ac.kr>
2024-04-30 22:50:52 +0900

    ui practice - remember

--- app/src/main/java/com/example/jetpack1/MainActivity.kt
+++ app/src/main/java/com/example/jetpack1/MainActivity.kt
@@ -15,6 +15,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
@@ -31,7 +35,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Jetpack1Theme {
-                MyBtn()
+                SimpleCounterBtn()
             }
         }
     }
@@ -74,6 +78,22 @@ fun MyBtn() {
                 fontSize = 30.sp,
                 color = Color.Red
             )
+    }
+}
+
+@Composable
+fun SimpleCounterBtn() {
+    var count by remember { mutableStateOf(0) }
+
+    Button(onClick = {
+        count++
+    },
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text(
+            text = "Count : $count",
+            fontSize = 50.sp
+        )
     }
 }
 
