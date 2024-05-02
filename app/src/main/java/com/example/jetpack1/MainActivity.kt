b506efad814e1dee34454548c5e79f8b8f227ea8
Eunchae Cho <choeunchae0828@gmail.com>
2024-05-02 15:39:44 +0900

    ui practice - box

--- app/src/main/java/com/example/jetpack1/MainActivity.kt
+++ app/src/main/java/com/example/jetpack1/MainActivity.kt
@@ -7,13 +7,16 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -48,7 +51,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Jetpack1Theme {
-                MyImageTest2()
+                BoxExample()
             }
         }
     }
@@ -255,10 +258,98 @@ fun MyImageTest2() {
     )
 }
 
+// Box : 적당히 레이아웃 항목을 겹쳐서 내가 원하는 위치에 배치할 수 있음
+@Composable
+fun BoxExample() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.Red)
+    ) {
+        Box(
+            modifier = Modifier
+                .size(100.dp)
+                .background(color = Color.Blue)
+                .padding(16.dp)
+                .align(Alignment.TopStart)
+        ) {
+            Text(text = "왼쪽 위")
+        }
+
+        Box(
+            modifier = Modifier
+                .size(100.dp)
+                .background(color = Color.Green)
+                .padding(16.dp)
+                .align(Alignment.TopCenter)
+        ) {
+            Text(text = "중앙 위")
+        }
+
+        Box(
+            modifier = Modifier
+                .size(100.dp)
+                .background(color = Color.Gray)
+                .padding(16.dp)
+                .align(Alignment.TopEnd)
+        ) {
+            Text(text = "오른쪽 위")
+        }
+
+        Button(
+            onClick = { },
+            modifier = Modifier.align(Alignment.CenterStart)
+        ) {
+            Text(text = "중앙 왼쪽")
+        }
+        Button(
+            onClick = { },
+            modifier = Modifier.align(Alignment.Center)
+        ) {
+            Text(text = "중앙 중앙")
+        }
+        Button(
+            onClick = { },
+            modifier = Modifier.align(Alignment.CenterEnd)
+        ) {
+            Text(text = "중앙 오른쪽")
+        }
+
+        Box(
+            modifier = Modifier
+                .size(100.dp)
+                .background(color = Color.Blue)
+                .padding(16.dp)
+                .align(Alignment.BottomStart)
+        ) {
+            Text(text = "왼쪽 아래")
+        }
+
+        Box(
+            modifier = Modifier
+                .size(100.dp)
+                .background(color = Color.Green)
+                .padding(16.dp)
+                .align(Alignment.BottomCenter)
+        ) {
+            Text(text = "중앙 아래")
+        }
+
+        Box(
+            modifier = Modifier
+                .size(100.dp)
+                .background(color = Color.Gray)
+                .padding(16.dp)
+                .align(Alignment.BottomEnd)
+        ) {
+            Text(text = "오른쪽 아래")
+        }
+    }
+}
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Jetpack1Theme {
-        MyImageTest2()
+        BoxExample()
     }
 }
