70a0ccd2b1cea204388d6c457daa4633ec2ef72f
Eunchae Cho <choeunchae0828@gmail.com>
2024-05-10 20:53:27 +0900

    ui practice - retrofit 2

--- app/src/main/java/com/example/jetpack1/MainActivity.kt
+++ app/src/main/java/com/example/jetpack1/MainActivity.kt
@@ -87,6 +87,7 @@ import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -95,9 +96,64 @@ class MainActivity : ComponentActivity() {
             Jetpack1Theme {
 
 //                MyNav2(navHostController = rememberNavController())
-                Retrofit()
+                InsertInputData()
             }
         }
+    }
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun InsertInputData() {
+
+        // 입력하는 숫자
+        var inputNumber by remember {
+            mutableStateOf("")
+        }
+
+        // 받아온 데이터 모델
+        var post by remember {
+            mutableStateOf<Post?>(null)
+        }
+
+        // API 통신하는 부분
+        val coroutineScope = rememberCoroutineScope()
+
+        Column (
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            TextField(
+                value = inputNumber,
+                onValueChange = { inputNumber = it},
+                label = { Text(text = "값을 입력해주세요 숫자를")}
+            )
+
+            Button(
+                onClick = {
+                    val number = inputNumber.toIntOrNull()
+                    if (number != null) {
+                        coroutineScope.launch {
+                            post = getPostData(number)
+                        }
+                    }
+                })
+            {
+                Text(text = "데이터 받아오기")
+            }
+            post?.let {
+                Text(text = "UserId : " + it.userId)
+                Text(text = "ID : " + it.id)
+                Text(text = "Title : " + it.title)
+                Text(text = "Body : " + it.body)
+            }
+        }
+    }
+
+    private suspend fun getPostData(number : Int) : Post? {
+        val retrofitInstance = RetrofitInstance.getInstance().create(MyApi::class.java)
+        val response = retrofitInstance.getPostNumber(number)
+        return if (response.isSuccessful) response.body() else null
     }
 }
 
@@ -1064,6 +1120,11 @@ interface MyApi {
 
     @GET("post/1")
     suspend fun getPost1() : Response<Post>
+
+    @GET("posts/{number}")
+    suspend fun getPostNumber(
+        @Path("number") number : Int
+    ) : Response<Post>
 }
 
 @Composable
